2299f68d39dbca64a4d57f6aa178c3efde346d0d
James <t397j696@home.ku.edu>
2026-04-30 21:30:05 -0500

    here we go

--- waveform_generator/waveform_generator.srcs/sources_1/new/PhaseAccumulator.vhd
+++ waveform_generator/waveform_generator.srcs/sources_1/new/PhaseAccumulator.vhd
@@ -33,7 +33,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity PhaseAccumulator is
     Port ( Clk : in STD_LOGIC;
-           Sel : in STD_LOGIC_VECTOR (1 downto 0);
+           Freq : in STD_LOGIC_VECTOR (1 downto 0);
            Phase : out STD_LOGIC_VECTOR (7 downto 0));
 end PhaseAccumulator;
 
