30e382c441aae123991ab6145d4cca7dd075c96e
Vincent Vordtriede <vwv0945@gmail.com>
2026-04-30 19:55:55 -0500

    Changed rect and tri file types to VHDL

---
+++ waveform_generator/waveform_generator.srcs/sources_1/new/Rectangle.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/30/2026 07:54:49 PM
+-- Design Name: 
+-- Module Name: Rectangle - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Rectangle is
+    Port ( Phase : in STD_LOGIC_VECTOR (7 downto 0);
+           Wav : out STD_LOGIC_VECTOR (7 downto 0));
+end Rectangle;
+
+architecture Behavioral of Rectangle is
+
+begin
+
+
+end Behavioral;
