eac6807e5afcb4a1416d06e8f37405ebe6da1b30
James <t397j696@home.ku.edu>
2026-05-04 09:48:39 -0500

    completed sim file

--- waveform_generator/waveform_generator.srcs/sim_1/new/Top_tb.vhd
+++ waveform_generator/waveform_generator.srcs/sim_1/new/Top_tb.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -43,26 +43,39 @@ component Top is
            clk : in STD_LOGIC);
 end component;
 
-signal freq, sel: std_logic_vector(1 downto 0):= "00";
-signal wav: std_logic_vector(7 downto 0);
+signal freq: std_logic_vector(1 downto 0):= "00";
+signal wav1, wav2, wav3, wav4: std_logic_vector(7 downto 0) := "00000000";
 signal clk: std_logic:= '0';
 
 begin
 
-UUT: Top 
-port map (clk => clk, freq => freq, wav => wav, sel => sel);
+UUT1: Top 
+port map (clk => clk, freq => freq, wav => wav1, sel => "00");
+
+UUT2: Top
+port map (clk => clk, freq => freq, wav => wav2, sel => "01");
+
+UUT3: Top
+port map (clk => clk, freq => freq, wav => wav3, sel => "10");
+
+UUT4: Top
+port map (clk => clk, freq => freq, wav => wav4, sel => "11");
 
 process
-		variable k : integer := 0;
+    variable k : integer := 0;
 	begin
-		
-		wait for 10 ns;
-		for i in 0 to 2000 loop
-            clk <= '1';
+	
+	   for i in 0 to 3 loop
             wait for 10 ns;
-            clk <= '0';
-            wait for 10 ns;
-        end loop;
+            for j in 0 to 500 loop
+                clk <= '1';
+                wait for 10 ns;
+                clk <= '0';
+                wait for 10 ns;
+            end loop;
+            freq <= std_logic_vector(unsigned(freq) + 1);
+	   end loop;
+	
 	end process;
 
 end Behavioral;
